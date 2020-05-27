40280fccd5d89dd651abfc467e1ee6a090ac1cba
Troy mangrum <tmmangrum@gmail.com>
2020-05-27 18:29:27 -0500

    got my api address

---
+++ src/components/pictureList.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import pictureCard from './pictureCard'
+
+
+
+const pictureList = () => {
+    const [photo, newPhoto] = useState({}); 
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=LyUAXHY7mEpHs5GdUDc43GhVhX2IUuuO8LC9HnP7')
+        .then(response => {
+            newPhoto(response.data)
+        })
+        
+    })
+}
