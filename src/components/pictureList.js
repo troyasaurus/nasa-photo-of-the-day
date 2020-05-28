21a7642a33b3461d08c57f5561566be58d60b945
Troy mangrum <tmmangrum@gmail.com>
2020-05-28 16:33:59 -0500

    added card information.

--- src/components/pictureList.js
+++ src/components/pictureList.js
@@ -1,17 +1,20 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import pictureCard from './pictureCard'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import pictureCard from './pictureCard';
+import { Card, CardTitle, CardText, CardImg, CardBody, Button } from 'reactstrap';
 
 
 
 const pictureList = () => {
     const [photo, newPhoto] = useState({}); 
+    const [date, setDate] = useState(""); 
 
     useEffect(() => {
         axios
         .get('https://api.nasa.gov/planetary/apod?api_key=LyUAXHY7mEpHs5GdUDc43GhVhX2IUuuO8LC9HnP7')
         .then(response => {
-            newPhoto(response.data)
+            newPhoto(response.data);
+            setDate(response.data.date);
         })
 
     }, []);
@@ -19,15 +22,15 @@ const pictureList = () => {
 
 
     return(
-        <div>
-            <pictureCard
-            key={photo.id}
-            date={photo.date}
-            title={photo.title}
-            description={photo.description}
-            picture={photo.url}/>
-        
-        </div>
+        <Card className="pictureContainer">
+          <CardImg src={pictureCard.url} alt="Nasa Photo of the Day"/>
+            <CardBody>
+                <CardTitle className="card">{photo.title}</CardTitle>
+                <CardText className="card">{photo.explanation}>{}</CardText>
+                <CardText className="card">{photo.date}>{}</CardText>
+                <Button className="button"></Button>
+            </CardBody>
+        </Card>
     )
 }
 
